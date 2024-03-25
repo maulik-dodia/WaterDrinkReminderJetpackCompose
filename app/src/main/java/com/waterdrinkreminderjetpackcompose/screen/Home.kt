9963578233b765795ff9776ff6176390a9cc5b3b
Maulikkumar Dodia <maulikkumardodia@MaulikkumarsMBP.domain.name>
2024-03-25 17:18:05 +0530

    Scaffold in home

--- app/src/main/java/com/waterdrinkreminderjetpackcompose/screen/Home.kt
+++ app/src/main/java/com/waterdrinkreminderjetpackcompose/screen/Home.kt
@@ -1,9 +1,46 @@
 package com.waterdrinkreminderjetpackcompose.screen
 
+import android.annotation.SuppressLint
 import android.net.wifi.hotspot2.pps.HomeSp
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.waterdrinkreminderjetpackcompose.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home() {
-
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(text = "Maulik")
+                })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Scaffold content"
+            )
+        }
+    }
 }
