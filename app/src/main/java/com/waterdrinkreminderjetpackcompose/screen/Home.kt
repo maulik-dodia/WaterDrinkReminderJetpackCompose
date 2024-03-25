d012ed2212077e86c3d7b8b53a02ccbca7692aec
Maulikkumar Dodia <maulikkumardodia@MaulikkumarsMBP.domain.name>
2024-03-25 18:03:40 +0530

    Update home ui

--- app/src/main/java/com/waterdrinkreminderjetpackcompose/screen/Home.kt
+++ app/src/main/java/com/waterdrinkreminderjetpackcompose/screen/Home.kt
@@ -2,9 +2,20 @@ package com.waterdrinkreminderjetpackcompose.screen
 
 import android.annotation.SuppressLint
 import android.net.wifi.hotspot2.pps.HomeSp
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -13,34 +24,58 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.waterdrinkreminderjetpackcompose.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(text = "Maulik")
-                })
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row(modifier = Modifier
+            .weight(0.1f)
+            .background(color = Color.Gray)){
+            Image(
+                painter = painterResource(id = R.drawable.profile_img),
+                contentDescription = stringResource(id = R.string.profile_img_small),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(shape = RoundedCornerShape(10.dp)))
+            Column(verticalArrangement = Arrangement.Center
+                ,modifier = Modifier
+                    .height(50.dp)
+                    .padding(start = 5.dp)) {
+                Text(
+                    text = stringResource(id = R.string.label_good_morning),
+                    fontWeight = FontWeight.Light
+                )
+                Text(
+                    text = stringResource(id = R.string.user_name),
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = "Scaffold content"
-            )
+        Column(modifier = Modifier
+            .weight(0.5f)
+            .background(color = Color.Green)) {
+            Text(text = "Maulik")
+        }
+        Column(modifier = Modifier
+            .weight(0.5f)
+            .background(color = Color.Green)) {
+            Text(text = "Maulik")
         }
     }
 }
